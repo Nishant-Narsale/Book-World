98cb75fed18b047be10d039eb6ffe544acbe6472
Nishant <nishantnarsale33@gmail.com>
2021-07-03 20:23:31 +0530

    Fixing deployment

--- mpbi/urls.py
+++ mpbi/urls.py
@@ -19,8 +19,7 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from django.views.static import serve
-from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,8 +29,6 @@ urlpatterns = [
 
 
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)   
