fc14a0671e944a3614e4f628520397f2e524ad54
Nishant <nishantnarsale33@gmail.com>
2021-07-04 09:20:27 +0530

    debuggin

--- mpbi/settings.py
+++ mpbi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '((wfxd&n&r3*cn^_#k%$tc36&(steb8+q7pz(=!t4_n^2a+$@y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1",'book-world-1.herokuapp.com']
 
@@ -158,6 +158,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
+# For production on Heroku
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WHITENOISE_USE_FINDERS=True
